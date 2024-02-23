a4ed643b2cc7652f1320ef9b608e961c9203ccac
Daniel Kuzminski <dankuzmina@gmail.com>
2024-02-23 04:50:50 +0100

    change to mobile app resolution, email details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,23 +6,23 @@ import logo from "../assets/logo.jpg"
 
 export default function Footer() {
 	return (
-		<div className='footer'>
-			<div className='contact'>
-				<p className='contact__details'>contact details: </p>
+		<div className="footer">
+			<div className="contact">
+				<p className="contact__details">contact details: </p>
 				<div>
-					<i class='fa-solid fa-phone'></i>
-					<a className='phone' href='tel:00353877759009'>
+					<i class="fa-solid fa-phone"></i>
+					<a className="phone" href="tel:00353877759009">
 						Call us +353877759009
 					</a>
 				</div>
 				<div>
-					<i class='fa-solid fa-envelope'></i>
-					<a className='phone' href='mailto:airpotaxi@airportaxi.ie'>
+					<i class="fa-solid fa-envelope"></i>
+					<a className="phone" href="mailto:bookings@airportaxi.ie">
 						Email us airpotaxi@airportaxi.ie
 					</a>
 				</div>
 			</div>
-			<img src={logo} alt='logo' className='logo' />
+			<img src={logo} alt="logo" className="logo" />
 		</div>
 	)
 }
